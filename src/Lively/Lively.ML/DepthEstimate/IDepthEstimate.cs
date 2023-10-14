8b6a39494bf44f1aa8b562dd3b865751fbc9b381
chinnu0505 <pechuri9@gmail.com>
2023-10-14 15:11:53 -0400

    clear

--- src/Lively/Lively.ML/DepthEstimate/IDepthEstimate.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Lively.ML.DepthEstimate
-{
-    public interface IDepthEstimate : IDisposable
-    {
-        void LoadModel(string path);
-        ModelOutput Run(string imagePath);
-        string ModelPath { get; }
-    }
-}
